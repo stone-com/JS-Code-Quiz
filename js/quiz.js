cad2d313236d7f7d461dbd7abf177b694a87f5d2
Stone Comstock <comstockstone@yahoo.com>
2022-01-11 22:58:34 -0800

    first commit - starter code

---
+++ js/quiz.js
@@ -0,0 +1,10 @@
+/*
+ * questions.js is loaded in the HTML before quiz.js
+ * It creates a global variable called questions that contains starter questions.
+ * Take a look at the structure and familiarize yourself with each part
+ * then, add some of your own questions!
+ * Use this data to populate your quiz questions, choices, and answers.
+ */
+console.log(questions);
+
+
